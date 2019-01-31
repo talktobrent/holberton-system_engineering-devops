cd2af245685bf0508919b57a588c0574925b7126
talktobrent <talktobrent@gmail.com>
2019-01-31 15:07:11 -0800

    Removed file already renamed

--- 0x14-mysql/1-mysql_backup.sh
+++
@@ -1,5 +0,0 @@
-#!/bin/bash
-# Dumps all mysql databases and compresses archive
-set -e
-mysqldump --all-databases --events -uroot -p"$1" > backup.sql
-tar -czvf "$(date '+%d-%m-%Y').tar.gz" backup.sql
