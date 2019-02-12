7cc5fb033cb973875bff521beed982a4e7e43ad9
talktobrent <talktobrent@gmail.com>
2019-02-11 17:53:08 -0800

    Created script

---
+++ 0x15-api/3-dictionary_of_list_of_dictionaries.py
@@ -0,0 +1,27 @@
+#!/usr/bin/python3
+""" requests all task list for user and exports to json file """
+
+import requests
+import json
+
+
+if __name__ == "__main__":
+
+    response = requests.get("https://jsonplaceholder.typicode.com/users")
+    users = response.json()
+    response = requests.get("https://jsonplaceholder.typicode.com/todos")
+    tasks = response.json()
+    user_dict = {}
+    task_list = []
+    for user in users:
+        for task in list(tasks):
+            if task.get("userId") == user.get("id"):
+                task_list.append({"username": user.get("username"),
+                                  "task": task.get("title"),
+                                  "completed": task.get("completed")})
+                tasks.remove(task)
+        user_dict.update({user.get("id"): task_list})
+        task_list = []
+
+    with open("todo_all_employees.json", 'w') as jfile:
+        json.dump(user_dict, jfile)
